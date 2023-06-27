10fe31932d224928ca986fae2553b841f4643999
ittp <61617660+ittp@users.noreply.github.com>
2023-06-27 07:17:45 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ const root = createRoot(rootElement);
 
 root.render(
   <>
-    <App />
-    <Content data={interfaces} />
+    <App
+      users={[
+        { id: 1, user: "tpa", group: "tpa" },
+        { id: 2, user: "admin", group: "admin" },
+        { id: 3, user: "view", group: "view" },
+        { id: 4, user: "disabled", disabled: true, group: "disabled" },
+      ]}
+    />
   </>
 );

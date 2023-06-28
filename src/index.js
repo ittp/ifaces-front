cebd45cea7e34c7cae09cfb1965f56bdacc707b6
ittp <61617660+ittp@users.noreply.github.com>
2023-06-28 01:38:31 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -7,15 +7,12 @@ import interfaces from "./demo.json";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+
+
 root.render(
   <>
-    <App
-      users={[
-        { id: 1, user: "tpa", group: "tpa" },
-        { id: 2, user: "admin", group: "admin" },
-        { id: 3, user: "view", group: "view" },
-        { id: 4, user: "disabled", disabled: true, group: "disabled" },
-      ]}
+    <App />
+     
     />
   </>
 );
